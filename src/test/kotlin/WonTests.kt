d335fc1d8f3e477391696a3625d17504ce3faad2
jinia91 <creee77@gmail.com>
2022-10-23 14:50:55 +0900

    value object 패턴이므로 패스

--- src/test/kotlin/WonTests.kt
+++ src/test/kotlin/WonTests.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
    ( )1. $5 + ₩1000 = (환율에따라 $1 = ₩1000) ₩6000
    (v)2. ₩5 * ₩2 = ₩20
    ( )3. amount 를 private으로 만들기
-   ( )4. ₩ 부작용?
+   (v)4. ₩ 부작용? = value class 사용! value object pattern!
    ( )5. Won 반올림?
  */
 class `통화가 다른 두 금액을 더해서 주어진 환율에 맞게 변한 금액을 결과로 얻을수 있어야한다` {
