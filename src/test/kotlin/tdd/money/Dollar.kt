a254c7120895fe0c57cdd4e8884baa7d68af47c0
jinia91 <creee77@gmail.com>
2023-11-21 20:08:22 +0900

    (3-3) GREEN: equeals, 구현, 테스트 성공

--- src/test/kotlin/tdd/money/Dollar.kt
+++ src/test/kotlin/tdd/money/Dollar.kt
@@ -2,4 +2,8 @@ package tdd.money
 
 class Dollar(var amount: Int) {
     fun times(multiplier: Int) = Dollar(amount * multiplier)
+
+    override fun equals(other: Any?): Boolean {
+        return other is Dollar && amount == other.amount
+    }
 }
