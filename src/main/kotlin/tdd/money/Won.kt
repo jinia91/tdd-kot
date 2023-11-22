d7ef810fcc54d4192a920cfb3360ac6e8cbbf2f3
jinia91 <creee77@gmail.com>
2023-11-22 16:53:53 +0900

    (6-4) GREEN: money, won , equals 중복 제거 리팩토링 완료 성공하는 테스트

--- src/main/kotlin/tdd/money/Won.kt
+++ src/main/kotlin/tdd/money/Won.kt
@@ -2,8 +2,4 @@ package tdd.money
 
 class Won(amount: Int) : Money(amount) {
     fun times(multiplier: Int) = Won(amount * multiplier)
-
-    override fun equals(other: Any?): Boolean {
-        return other is Won && amount == other.amount
-    }
 }
