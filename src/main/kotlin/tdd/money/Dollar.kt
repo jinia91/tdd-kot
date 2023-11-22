185acd4fd783a334c0bf4c26ef5b2b223ac38053
jinia91 <creee77@gmail.com>
2023-11-22 15:32:24 +0900

    (6-1) GREEN: money, 슈퍼 클래스 선언과 상속 리팩토링 - 여전히 GREEN!

--- src/main/kotlin/tdd/money/Dollar.kt
+++ src/main/kotlin/tdd/money/Dollar.kt
@@ -1,6 +1,6 @@
 package tdd.money
 
-class Dollar(private val amount: Int) {
+class Dollar(private val amount: Int) : Money() {
     fun times(multiplier: Int) = Dollar(amount * multiplier)
 
     override fun equals(other: Any?): Boolean {
