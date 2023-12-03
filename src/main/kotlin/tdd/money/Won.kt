ce8d172fa0ecf5bbfc596d9c4f1608afab8ecd26
jinia91 <creee77@gmail.com>
2023-12-03 13:29:38 +0900

    (10-4) GREEN: 원화 리팩토링 추가 및 성공 테스트

--- src/main/kotlin/tdd/money/Won.kt
+++ src/main/kotlin/tdd/money/Won.kt
@@ -3,5 +3,5 @@ package tdd.money
 class Won(
     amount: Int
 ) : Money(amount, "KRW") {
-    override fun times(multiplier: Int): Money = Won(amount * multiplier)
+    override fun times(multiplier: Int): Money = Money(amount * multiplier, this.currency)
 }
