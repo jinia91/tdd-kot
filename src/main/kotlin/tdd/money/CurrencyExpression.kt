b011695282f0deaae2f96325bd1b86293306fdfe
jinia91 <creee77@gmail.com>
2023-12-03 15:18:49 +0900

    (16-3) RED: 삼중 곱셈 성공 테스트

--- src/main/kotlin/tdd/money/CurrencyExpression.kt
+++ src/main/kotlin/tdd/money/CurrencyExpression.kt
@@ -4,11 +4,17 @@ interface CurrencyExpression{
     fun reduce(bank : Bank,to: String): Money
 
     operator fun plus(addend: CurrencyExpression): CurrencyExpression = Sum(this, addend)
+
+    operator fun times(multiplier: Int): CurrencyExpression
 }
 
 class Sum(private val augend: CurrencyExpression, private val addend: CurrencyExpression) : CurrencyExpression {
     override fun reduce(bank: Bank, to: String): Money {
         val amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount
         return Money(amount, to)
+    }
+
+    override fun times(multiplier: Int): CurrencyExpression {
+        return Sum(augend * multiplier, addend * multiplier)
     }
 }
