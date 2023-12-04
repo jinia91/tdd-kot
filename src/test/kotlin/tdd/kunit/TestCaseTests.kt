e04e21819d55175de760a78b13d3082b5197f49c
jinia91 <creee77@gmail.com>
2023-12-04 18:43:17 +0900

    (21-1) RED: 테스트 report 실패하는 테스트 작성

--- src/test/kotlin/tdd/kunit/TestCaseTests.kt
+++ src/test/kotlin/tdd/kunit/TestCaseTests.kt
@@ -14,4 +14,11 @@ class TestCaseTests(testName: String) : TestCase(testName) {
         check(test.log.contains("testMethod")){ "testMethod가 실행되어야 합니다."}
         check(test.log.last() == "tearDown") { "tearDown이 마지막에 실행되어야 합니다."}
     }
+
+    fun `테스트를 수행하고나면 테스트 결과를 확인 할 수 있다`() {
+        test = WasRun("testMethod")
+        test.run()
+        check(test.result.run == 1) { "테스트 결과가 다릅니다."}
+        check(test.result.fail == 0) { "테스트 결과가 다릅니다."}
+    }
 }
