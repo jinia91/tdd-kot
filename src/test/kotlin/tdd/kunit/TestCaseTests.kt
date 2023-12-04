22337387de39a3087ba7b7db1f585d897467e3d5
jinia91 <creee77@gmail.com>
2023-12-04 18:32:21 +0900

    (20-3) RED: teardown 실패하는 테스트 및 assert 대신 check로 변경

--- src/test/kotlin/tdd/kunit/TestCaseTests.kt
+++ src/test/kotlin/tdd/kunit/TestCaseTests.kt
@@ -9,7 +9,8 @@ class TestCaseTests(testName: String) : TestCase(testName) {
     fun `테스트케이스템플릿이 정상동작한다`() {
         test = WasRun("testMethod")
         test.run()
-        assert(test.log.first() == "setUp")
-        assert(test.log.contains("testMethod"))
+        check(test.log.first() == "setUp") { "setUp이 먼저 실행되어야 합니다."}
+        check(test.log.contains("testMethod")){ "testMethod가 실행되어야 합니다."}
+        check(test.log.last() == "tearDown") { "tearDown이 마지막에 실행되어야 합니다."}
     }
 }
