b596e980bdcbe6ad395bf55120057b2fc00f397f
jinia91 <creee77@gmail.com>
2023-12-04 18:10:01 +0900

    (19-1) RED: 테스트케이스 setUp시 세팅 컴파일 실패하는 테스트

--- src/test/kotlin/tdd/kunit/TestCaseTests.kt
+++ src/test/kotlin/tdd/kunit/TestCaseTests.kt
@@ -2,6 +2,12 @@ package tdd.kunit
 
 class TestCaseTests(testName: String) : TestCase(testName) {
 
+    fun `테스트케이스를 setUp 하면 새로 세팅된다`() {
+        val test = WasRun("testMethod")
+        test.run()
+        assert(test.wasSetUp)
+    }
+
     fun `테스트케이스를 run 하면 wasRun이 된다`() {
         val test = WasRun("testMethod")
         assert(!test.wasRun)
