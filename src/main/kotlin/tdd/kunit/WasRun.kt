1d55a64ca25c89425abcf38f3e63d737dd24859a
jinia91 <creee77@gmail.com>
2023-12-04 18:20:58 +0900

    (20-1) GREEN: flag -> log로 리팩토링 성공하는 테스트

--- src/main/kotlin/tdd/kunit/WasRun.kt
+++ src/main/kotlin/tdd/kunit/WasRun.kt
@@ -4,13 +4,16 @@ class WasRun(
     name: String,
     var wasRun: Boolean = false,
     var wasSetUp: Boolean = false,
+    val log: MutableList<String> = mutableListOf()
 ) : TestCase(name){
-    fun testMethod(){
-        wasRun = true
-    }
 
     override fun setUp() {
         wasRun = false
         wasSetUp = true
+        log += "setUp"
+    }
+
+    fun testMethod(){
+        wasRun = true
     }
 }
--- src/test/kotlin/tdd/kunit/TestCaseTests.kt
+++ src/test/kotlin/tdd/kunit/TestCaseTests.kt
@@ -9,7 +9,7 @@ class TestCaseTests(testName: String) : TestCase(testName) {
 
     fun `테스트케이스를 setUp 하면 새로 세팅된다`() {
         test.run()
-        assert(test.wasSetUp)
+        assert(test.log.first() == "setUp")
     }
 
     fun `테스트케이스를 run 하면 wasRun이 된다`() {
