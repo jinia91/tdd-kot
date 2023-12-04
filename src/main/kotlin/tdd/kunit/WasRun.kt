f4e2ecbe941ec177226772be1408297db7ca3f6f
jinia91 <creee77@gmail.com>
2023-12-04 18:15:07 +0900

    (19-2) GREEN: 테스트케이스 setUp시 세팅 구현 및 성공 테스트

--- src/main/kotlin/tdd/kunit/WasRun.kt
+++ src/main/kotlin/tdd/kunit/WasRun.kt
@@ -3,8 +3,14 @@ package tdd.kunit
 class WasRun(
     name: String,
     var wasRun: Boolean = false,
+    var wasSetUp: Boolean = false,
 ) : TestCase(name){
     fun testMethod(){
         wasRun = true
+    }
+
+    override fun setUp() {
+        wasRun = false
+        wasSetUp = true
     }
 }
